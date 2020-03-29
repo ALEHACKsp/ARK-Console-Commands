de8457530fca9747ef8e55ee2257ea83aae73e88
doug147 <douglorenzen@gmail.com>
2020-03-29 17:58:52 -0400

    Update Console.hpp

--- Console.hpp
+++ Console.hpp
@@ -14,10 +14,11 @@ class Console
 {
 private:
 	static std::deque<std::string> Split(const std::string& s, char delimiter);
-	static std::unordered_map <std::string, std::function<void(std::deque<std::string>)>> Commands;
+	static std::unordered_map <std::string, std::function<void(std::deque<std::string>)>> c0;
+	static std::unordered_map <std::string, std::function<void()>> c1;
 	static void Process(std::deque<std::string>);
 public:
 	static void Set(std::string, std::function<void(std::deque<std::string>)>);
+	static void Set(std::string, std::function<void()>);
 	static void ConsoleCommand(std::string);
-	static void ToString();
 };
